1996c0c6aa35a5560bcf401a781ee3e8eba5809c
quang <quang@dfir.vn>
2022-11-04 13:35:08 +0700

    run

---
+++ deploy.sh
@@ -0,0 +1,1 @@
+docker run --name myadmin -d -e PMA_HOST=ltrandb.cxo9vggtgdxv.ap-southeast-1.rds.amazonaws.com -e PMA_USER=airtimeOverview -e PMA_PASSWORD=2f96172b071e407dd899e88b29afa2b4 -p 8080:80 phpmyadmin:testing
